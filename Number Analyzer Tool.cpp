7ce75c99eedb7973d3846b05e312be294f65afd7
UncleH25 <oneilljarryd@gmail.com>
2025-06-06 05:18:10 +0200

    Added initial variables.

--- Number Analyzer Tool.cpp
+++ Number Analyzer Tool.cpp
@@ -6,7 +6,10 @@ using namespace std;
 
 int main()
 {
-    
+    //Variables
+    int number;
+	int count = 0, even = 0, odd = 0, positive = 0, negative = 0;
+	int max = 0, min = 0;
 
     return 0;
 }
