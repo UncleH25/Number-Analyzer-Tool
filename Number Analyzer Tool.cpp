f67adafc73f03d56de634a3e2b80919acfe82c14
UncleH25 <oneilljarryd@gmail.com>
2025-06-06 05:20:29 +0200

    Added input to prompt the user to eneter any positive or negative whole number.

--- Number Analyzer Tool.cpp
+++ Number Analyzer Tool.cpp
@@ -11,5 +11,8 @@ int main()
 	int count = 0, even = 0, odd = 0, positive = 0, negative = 0;
 	int max = 0, min = 0;
 
+    //Input
+	cout << "Enter a number (positive or negative): ";
+
     return 0;
 }
